8333aa4b1e4e0e5669cf36c1eb610e142c721cd9
Anthony <anthony.key@talkout.com>
2022-07-14 10:07:16 +0100

    Initial commit, Added Categories model & setup connection string to database

---
+++ AKeyBook/AKeyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AKeyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKeyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ AKeyBook/AKeyBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AKeyBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

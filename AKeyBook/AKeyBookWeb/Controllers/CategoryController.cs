4bc96324112c0d53700b155cc4541f5f164bb6fc
Anthony <anthony.key@talkout.com>
2022-07-14 11:16:12 +0100

    Added migration for database, added temporary data & added bootstrap watch/ icons

---
+++ AKeyBook/AKeyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using AKeyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKeyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
